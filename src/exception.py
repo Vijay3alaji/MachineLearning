8bec7287ae90a24fd83286f199f4a4b12d22587a
Vijay <vijaybalaji237@gmail.com>
2026-04-16 03:50:26 +0530

    Add project structure and logging setup

---
+++ src/exception.py
@@ -0,0 +1,17 @@
+import sys
+
+def error_message(error, error_details:sys):
+    _, _, exc_tb = error_details.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = f"Error occurred in file: {file_name} at line number: {line_number} with error message: {str(error)}"
+    return error_message
+    
+class CustomException(Exception):
+    def __init__(self, error_message, error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message
+        self.error_details = error_details
+
+    def __str__(self):
+        return error_message(self.error_message, self.error_details)
---
+++ src/logging_config.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from datetime import datetime
+
+# Step 1: create logs folder
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+# Step 2: create log file name
+log_file = f"log_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+# Step 3: FULL correct path (only once!)
+LOG_FILE_PATH = os.path.join(LOG_DIR, log_file)
+
+# Step 4: configure logging
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+logging.info("Logging is working")
